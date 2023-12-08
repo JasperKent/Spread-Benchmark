f1e214ca36ad9e47ce3d2c42c8d90d4f41da7635
Jasper Kent <software@jasperkent.com>
2023-12-08 08:54:00 +0000

    Add project files.

---
+++ SpreadBenchmark/Program.cs
@@ -0,0 +1,36 @@
+﻿//int[] curly1 = { };
+//int[] curly2 = { };
+//int[] new1 = new int[0];
+//int[] new2 = new int[0];
+//int[] empty1 = Array.Empty<int>();
+//int[] empty2 = Array.Empty<int>();
+//int[] square1 = [];
+//int[] square2 = [];
+
+//Console.WriteLine(curly1 == curly2);
+//Console.WriteLine(new1 == new2);
+//Console.WriteLine(empty1 == empty2);
+//Console.WriteLine(square1 == square2);
+//Console.WriteLine(square1 == empty1);
+
+//string sempty1 = string.Empty;
+//string sempty2 = string.Empty;
+//string blank1 = "";
+//string blank2 = "";
+
+//Console.WriteLine(ReferenceEquals(sempty1, sempty2));
+//Console.WriteLine(ReferenceEquals(blank1, blank2));
+//Console.WriteLine(ReferenceEquals(sempty1, blank2));
+
+//int[] a1 = [1, 2, 3, 4, 5];
+
+//int[] a2 = [.. a1];
+
+//a2[2] = 500;
+
+//Console.WriteLine(a1[2]);
+
+using BenchmarkDotNet.Running;
+using SpreadBenchmark;
+
+BenchmarkRunner.Run<Tests>();
---
+++ SpreadBenchmark/Tests.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadBenchmark
+{
+    [MemoryDiagnoser]
+    public class Tests
+    {
+        private readonly int[] _data = new int[10_000];
+
+        [Benchmark(Baseline = true)]
+        public int[] WithToArray()
+        {
+            int[] results = _data.ToArray();
+
+            return results;
+        }
+
+        [Benchmark]
+        public int[] WithCopyTo()
+        {
+            int[] results = new int[_data.Length];
+
+            _data.CopyTo(results, 0);
+
+            return results;
+        }
+
+        [Benchmark]
+        public int[] WithSpread()
+        {
+            int[] results = [.._data];
+
+            return results;
+        }
+    }
+}
